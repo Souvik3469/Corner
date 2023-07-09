cd176805a4488e23c73acea3dc7fae5e71cd9f29
Ankush Banerjee <banerjeeankush184@gmail.com>
2023-07-09 18:27:50 +0530

    fixed the cards

---
+++ src/frontend/components/Card.js
@@ -0,0 +1,27 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+function CarCard() {
+  return (
+    <div className='bg-gray-200 w-[350px] flex flex-col p-3 rounded-lg  hover:bg-blue-200 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'>
+        <div className='flex items-center justify-center' >
+        <img className='' src='https://cdn.bigboytoyz.com/new-version/products/product/2016-ford-mustang-gt-black-6312.jpg '/>
+        </div>
+        <div className='font-bold text-lg my-2 '>
+           Ford Mustang Gt limited Edition
+        </div>
+        <div>
+    <Button
+    className='bg-blue-700 text-white'
+    variant="contained"
+    size="medium"
+
+    >
+    Buy for 7.5 ETH
+    </Button>
+        </div>
+    </div>
+  )
+}
+
+export default CarCard
--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -1,24 +1,27 @@
-import { useState, useEffect } from 'react'
-import { ethers } from "ethers"
-import { Row, Col, Card, Button } from 'react-bootstrap'
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { Row, Col, Card, Button } from "react-bootstrap";
+import CarCard from "./Card";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const Home = ({ marketplace, nft }) => {
-  const [loading, setLoading] = useState(true)
-  const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [items, setItems] = useState([]);
   const loadMarketplaceItems = async () => {
     // Load all unsold items
-    const itemCount = await marketplace.itemCount()
-    let items = []
+    const itemCount = await marketplace.itemCount();
+    let items = [];
     for (let i = 1; i <= itemCount; i++) {
-      const item = await marketplace.items(i)
+      const item = await marketplace.items(i);
       if (!item.sold) {
         // get uri url from nft contract
-        const uri = await nft.tokenURI(item.tokenId)
-        // use uri to fetch the nft metadata stored on ipfs 
-        const response = await fetch(uri)
-        const metadata = await response.json()
+        const uri = await nft.tokenURI(item.tokenId);
+        // use uri to fetch the nft metadata stored on ipfs
+        const response = await fetch(uri);
+        const metadata = await response.json();
         // get total price of item (item price + fee)
-        const totalPrice = await marketplace.getTotalPrice(item.itemId)
+        const totalPrice = await marketplace.getTotalPrice(item.itemId);
         // Add item to items array
         items.push({
           totalPrice,
@@ -26,60 +29,102 @@ const Home = ({ marketplace, nft }) => {
           seller: item.seller,
           name: metadata.name,
           description: metadata.description,
-          image: metadata.image
-        })
+          image: metadata.image,
+        });
       }
     }
-    setLoading(false)
-    setItems(items)
-  }
+    setLoading(false);
+    setItems(items);
+  };
 
   const buyMarketItem = async (item) => {
-    await (await marketplace.purchaseItem(item.itemId, { value: item.totalPrice })).wait()
-    loadMarketplaceItems()
-  }
+    await (
+      await marketplace.purchaseItem(item.itemId, { value: item.totalPrice })
+    ).wait();
+    loadMarketplaceItems();
+  };
 
   useEffect(() => {
-    loadMarketplaceItems()
-  }, [])
-  if (loading) return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Loading...</h2>
-    </main>
-  )
+    loadMarketplaceItems();
+  }, []);
+  if (loading)
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Loading...</h2>
+      </main>
+    );
   return (
-    <div className="flex justify-center">
-      {items.length > 0 ?
-        <div className="px-5 container">
-          <Row xs={1} md={2} lg={4} className="g-4 py-5">
-            {items.map((item, idx) => (
-              <Col key={idx} className="overflow-hidden">
-                <Card>
-                  <Card.Img variant="top" src={item.image} />
-                  <Card.Body color="secondary">
-                    <Card.Title>{item.name}</Card.Title>
-                    <Card.Text>
-                      {item.description}
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Footer>
-                    <div className='d-grid'>
-                      <Button onClick={() => buyMarketItem(item)} variant="primary" size="lg">
-                        Buy for {ethers.utils.formatEther(item.totalPrice)} ETH
-                      </Button>
-                    </div>
-                  </Card.Footer>
-                </Card>
-              </Col>
-            ))}
-          </Row>
+    // <div className="flex justify-center">
+    //   {items.length > 0 ?
+    //     <div className="px-5 container">
+    //       <Row xs={1} md={2} lg={4} className="g-4 py-5">
+    //         {items.map((item, idx) => (
+    //           <Col key={idx} className="overflow-hidden">
+    //             <Card>
+    //               <Card.Img variant="top" src={item.image} />
+    //               <Card.Body color="secondary">
+    //                 <Card.Title>{item.name}</Card.Title>
+    //                 <Card.Text>
+    //                   {item.description}
+    //                 </Card.Text>
+    //               </Card.Body>
+    //               <Card.Footer>
+    //                 <div className='d-grid'>
+    //                   <Button onClick={() => buyMarketItem(item)} variant="primary" size="lg">
+    //                     Buy for {ethers.utils.formatEther(item.totalPrice)} ETH
+    //                   </Button>
+    //                 </div>
+    //               </Card.Footer>
+    //             </Card>
+    //           </Col>
+    //         ))}
+    //       </Row>
+    //     </div>
+    //     : (
+    //       <main style={{ padding: "1rem 0" }}>
+    //         <h2>No listed assets</h2>
+    //       </main>
+    //     )}
+    // </div>
+    <div className="p-10">
+      <div className="flex items-center">
+      
+          <img
+            className="w-2/3 h-2/3"
+            src="https://imgd.aeplcdn.com/0x0/n/cw/ec/51378/s5-sportback-exterior-right-front-three-quarter-5.jpeg"
+          />
+        
+      
+        <div>
+          <div>
+            <h1 className="text-6xl font-bold text-blue-500 ">Decentralized</h1>
+          </div>
+        <div className="flex text-6xl font-bold justify-around gap-2 ">
+            <h1 className="text-4xl font-bold text-orange-500">Buy </h1>
+            
+            <h1 className="text-4xl font-semibold text-blue-500">&</h1>
+        
+            
+            <h1 className="text-4xl font-bold  text-orange-500">Sell</h1>
+
+          </div>
+          <div>
+            <h1 className="text-6xl font-bold text-blue-500  ">Cars</h1>
+          </div>
         </div>
-        : (
-          <main style={{ padding: "1rem 0" }}>
-            <h2>No listed assets</h2>
-          </main>
-        )}
+        </div>
+    
+      <div className="grid gap-3 grid-cols-4 ">
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+      </div>
     </div>
   );
-}
-export default Home
+};
+export default Home;
